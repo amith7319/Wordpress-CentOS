832973efd30c94e30ff6b552015a4dfdd347da9e
Amit Halder <36619176+amith7319@users.noreply.github.com>
2020-12-28 12:58:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,145 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+read -p "Enter the system hostname:  " WORDPRESSSITE
 
+yum update -y
+yum install wget epel-release curl nano -y
+
+# Installing Nginx
+
+if [ ! -x /usr/sbin/nginx ];
+    then
+        echo "NGINX will be INSTALLED now"
+        yum install nginx -y
+        systemctl start nginx
+        systemctl enable nginx
+    else
+        echo -----------------------------------------------------------------------------
+        echo "NGINX is already INSTALLED"
+        echo -----------------------------------------------------------------------------
+fi
+
+# Database environment creation
+MYSQLROOT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+echo Mysql root password = $MYSQLROOT >> /root/LEMPpassword.txt
+WPDATABASE=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+echo Wordpress database name = $WPDATABASE >> /root/LEMPpassword.txt
+WPUSER=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+echo Wordpress password = $WPUSER >> /root/LEMPpassword.txt
+WPPASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 20 | head -n 1)
+echo Wordpress password = $WPPASSWORD >> /root/LEMPpassword.txt
+
+## Installing Mariadb and Database setup for Wordpress
+if [ ! -x /usr/bin/mysql ];
+   then
+      echo "MARIADB will be INSTALLED now"
+      yum -y install mariadb mariadb-server
+      systemctl start mariadb
+      systemctl enable mariadb
+      mysql_secure_installation <<EOF
+      
+      y
+      $MYSQLROOT
+      $MYSQLROOT
+      y
+      n
+      y
+      y
+      EOF
+      
+   else
+      echo -----------------------------------------------------------------------------
+      echo "MARIADB is already INSTALLED"
+      echo -----------------------------------------------------------------------------
+      read -p "Enter the Mysql root password:  " EXISTINGPASSWORD
+      mysql -u root -p $EXISTINGPASSWORD
+fi
+
+
+# Installing Nginx
+
+if [ ! -x /usr/sbin/nginx ];
+    then
+        echo "NGINX will be INSTALLED now"
+        yum install nginx -y
+        systemctl start nginx
+        systemctl enable nginx
+    else
+        echo -----------------------------------------------------------------------------
+        echo "NGINX is already INSTALLED"
+        echo -----------------------------------------------------------------------------
+fi
+
+
+## Installing PHP
+
+if [ ! -x /usr/bin/php ];
+   then
+      echo "PHP will be INSTALLED now"
+      yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
+      yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64 |grep php
+      echo -----------------------------------------------------------------------------
+      read -p "Select one PHP version from above...like php70,php71,php80  " PHPV
+      echo -----------------------------------------------------------------------------
+      yum-config-manager --enable remi-$PHPV
+      yum install php php-fpm php-opcache php-cli php-gd php-curl php-mysql -y
+   else
+      echo -----------------------------------------------------------------------------
+      echo "PHP is already installed"
+      echo -----------------------------------------------------------------------------
+fi
+
+# Downloading Wordpress
+sudo mkdir -p /var/www/html/$WORDPRESSSITE
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar xf latest.tar.gz
+mv /tmp/wordpress/* /var/www/html/$WORDPRESSSITE
+chown -R nginx: /var/www/html/$WORDPRESSSITE
+
+#Changing PHP-FPM according to Nginx
+
+sed -i 's|user = apache|user = nginx|g' /etc/php-fpm.d/www.conf
+sed -i 's|group = apache|group = nginx|g' /etc/php-fpm.d/www.conf
+sed -i 's|;listen.owner = nobody|listen.owner = nginx|g' /etc/php-fpm.d/www.conf
+sed -i 's|;listen.group = nobody|listen.group = nginx|g' /etc/php-fpm.d/www.conf
+sed -i 's|listen = 127.0.0.1:9000|listen = /run/php-fpm/www.sock|g' /etc/php-fpm.d/www.conf
+sed -i 's|;listen.mode = 0660|listen.mode = 0666|g' /etc/php-fpm.d/www.conf
+chown -R root:nginx /var/lib/php
+systemctl enable php-fpm
+systemctl start php-fpm
+
+#Configuring TEST block NGINX
+
+IP=$(curl checkip.amazonaws.com)
+touch /etc/nginx/conf.d/$WORDPRESSSITE.conf
+cat > /etc/nginx/conf.d/$WORDPRESSSITE.conf << EOL
+server {
+    listen       80;
+    server_name  $IP $HOSTNAME;
+    root   /usr/share/nginx/html;
+    index index.php index.html index.htm;
+    
+    location ~ \.php$ {
+    fastcgi_pass unix:/run/php-fpm/www.sock;
+    fastcgi_index index.php;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include fastcgi_params;
+  }
+}
+EOL
+sed -i 's|fastcgi_param SCRIPT_FILENAME ;|fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;|g' /etc/nginx/conf.d/default.conf
+touch /usr/share/nginx/html/info.php
+cat > /usr/share/nginx/html/info.php << EOL
+<?php
+phpinfo();
+?>
+EOL
+
+systemctl restart nginx
+
+echo ...............................Finished...Installation....!!!
+echo " Visit.... http://$IP/info.php"
